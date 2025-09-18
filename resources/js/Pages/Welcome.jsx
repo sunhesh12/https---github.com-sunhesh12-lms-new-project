4ba6299e3978f9d8bb75126daa78c4ee205f1d4a
sunhesh12 <hsdbandaranayake@gmail.com>
2025-09-18 19:28:07 +0530

    update welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import style from "@/css/welcome.module.css";
 
 export default function Welcome() {
-
-
     return (
         <>
-            <h1>hello this is landing page of Laravel project</h1>
+            <div className={style["welcome-navbar-container"]}>
+                <div className={style["welcome-navbar-front-div"]}>
+                    <div>Home</div>
+                    <div>Contact</div>
+                    <div>Need help</div>
+                </div>
+                <div className={style["welcome-navbar-right-div"]}>
+                    <div className={style["welcome-navbar-right-logo-div"]}>
+                        <img src="/images/usjp-logo.png" alt="USJ Logo" />
+                    </div>
+                    <div className={style["welcome-navbar-right-text-div"]}>
+                        <h1>University of Sri Jayewardenepura</h1>
+                        <p>Learning Management System</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
