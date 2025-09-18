2bb9c57d7df75aa72fc0f3b26ed37f8973fea309
sunhesh12 <hsdbandaranayake@gmail.com>
2025-09-18 23:01:42 +0530

    update Landing Page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import style from "@/css/welcome.module.css";
-import LandingLayout from "@/Layouts/LandingLayout";
 import GuestLayout from "@/Layouts/GuestLayout";
+import { Button } from "@/components/ui/button"
+// import '@/../css/app.css';
+
 
 export default function Welcome() {
     return (
         <>
         <GuestLayout>
-            <div></div>
+            <div className={style["welcome-children-main-container"]}>
+                <div className={style["welcome-main-left-div"]}>
+                    <img src="./images/welcome-image.png" alt="Welcome Image" className={style["welcome-main-image"]} />
+                </div>
+                <div className={style["welcome-main-right-div"]}>
+                    <div className={style["welcome-main-right-text-h1"]}>
+                        <h1>Organize. Learn. <span>Create.</span></h1>
+                        <p>Welcome to the learning management System <br></br>
+                            of the <b>University of Sri Jayawardenepura.</b>
+                        </p>
+                        <div className={style["welcome-main-buttons"]}>
+                             <Button >Sign In</Button>
+                             <Button >Join LMS</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </GuestLayout>
         </>
     );
