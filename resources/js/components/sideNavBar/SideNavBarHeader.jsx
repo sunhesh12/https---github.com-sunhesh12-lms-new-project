c3b0258a0bd87b91d9f7787721501dc45c41d2ff
sunhesh12 <hsdbandaranayake@gmail.com>
2025-09-30 14:47:18 +0530

    fix side nav bar header

--- resources/js/components/sideNavBar/SideNavBarHeader.jsx
+++ resources/js/components/sideNavBar/SideNavBarHeader.jsx
@@ -1,21 +1,39 @@
 // src/components/sideNavBar/SideNavBarHeader.jsx
 import React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import style from "@/css/dashboard.module.css";
+// import style from "@/css/dashboard.module.css";
+import style from "@/css/sideNavBar.module.css";
 
 export default function SideNavBarHeader({ isOpen, toggleSidebar }) {
-  return (
-    <div className={style["sideNavBar-header"]}>
-      <img
-        src="/logo.png"
-        alt="University Logo"
-        className={style["sideNavBar-logo"]}
-      />
-      {!isOpen && <h2 className={style["sideNavBar-title"]}>My University</h2>}
-
-      <button className={style["sideNavBar-toggle"]} onClick={toggleSidebar}>
-        {isOpen ? <ChevronRight size={20} /> : <ChevronLeft size={20} />}
-      </button>
-    </div>
-  );
+    return (
+        <div className={style["sideNavBar-header"]}>
+            <img
+                src="/images/usjp-logo.png"
+                alt="University Logo"
+                className={style["sideNavBar-logo"]}
+            />
+            <div>
+                {!isOpen && (
+                    <p className={style["sideNavBar-header-title"]}>
+                        University Of Sri Jayawardenepura
+                    </p>
+                )}
+                {!isOpen && (
+                    <p className={style["sideNavBar-header-sub-title"]}>
+                        Faculty of Computing
+                    </p>
+                )}
+            </div>
+            <button
+                className={style["sideNavBar-toggle"]}
+                onClick={toggleSidebar}
+            >
+                {isOpen ? (
+                    <ChevronRight size={25} />
+                ) : (
+                    <ChevronLeft size={25} />
+                )}
+            </button>
+        </div>
+    );
 }
